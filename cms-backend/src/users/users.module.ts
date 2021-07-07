a28b6225a6b4eaa6103e4b8747f45ead3cceeb1a
Payrouse <leonardobone99@gmail.com>
2021-07-06 22:38:16 -0500

    Se creo los modelos de journalist complaint

--- cms-backend/src/users/users.module.ts
+++ cms-backend/src/users/users.module.ts
@@ -1,12 +1,18 @@
+import { Journalist } from './entities/journalist.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { User } from './entities/user.entity';
+import { JournalistsController } from './controllers/journalists.controller';
+import { JournalistsService } from './services/journalists.service';
+import { ComplaintsController } from './controllers/complaints.controller';
+import { ComplaintsService } from './services/complaints.service';
+import { Complaints } from './entities/complaints.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User, Journalist, Complaints])],
+  providers: [UsersService, JournalistsService, ComplaintsService],
+  controllers: [UsersController, JournalistsController, ComplaintsController],
 })
 export class UsersModule {}
