5c73ee29b1224a61d1960c4c40d77d21d6eb2bb5
Payrouse <leonardobone99@gmail.com>
2021-08-18 21:30:15 -0500

    Agrego servicio y controlador al comments

--- cms-backend/src/comments/controllers/comments.controller.ts
+++ cms-backend/src/comments/controllers/comments.controller.ts
@@ -1,12 +1,24 @@
+import { ApiKeyGuard } from 'src/auth/guards/api-key.guard';
 import { CommentsService } from './../services/comments.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+@UseGuards(ApiKeyGuard, JwtAuthGuard, RolesGuard)
 @Controller('comments')
 export class CommentsController {
   constructor(private commentsService: CommentsService) {}
 
+  @Public()
   @Get()
   getComments() {
     return this.commentsService.findAll();
+  }
+
+  @Public()
+  @Get()
+  getCommentByArticle(@Param(':articleId') id: string) {
+    return this.commentsService.findAllByArticle(id);
   }
 }
